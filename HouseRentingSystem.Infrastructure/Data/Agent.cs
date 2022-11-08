c15c101a8d58224465aa153f90834e65e56c3bb0
Valentin <valkata033@abv.bg>
2022-11-08 12:11:47 +0200

    Add models and seed data

---
+++ HouseRentingSystem.Infrastructure/Data/Agent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Infrastructure.Data
+{
+    public class Agent
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = null!;
+
+        public IdentityUser User { get; set; } = null!;
+
+    }
+}
--- HouseRentingSystem.Infrastructure/Data/ApplicationDbContext.cs
+++ HouseRentingSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HouseRentingSystem.Infrastructure.Data.Configuration;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HouseRentingSystem.Infrastructure.Data
@@ -8,6 +9,23 @@ namespace HouseRentingSystem.Infrastructure.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Agent> Agents { get; set; }
+
+        public DbSet<House> Houses { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new AgentConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new HouseConfiguration());
+
+
+            base.OnModelCreating(builder);
         }
     }
 }
